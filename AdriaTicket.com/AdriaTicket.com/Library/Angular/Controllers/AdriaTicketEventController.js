11c17832e894ad6e5e645e0036e933d217590329
iandelic <andelic.ivan@gmail.com>
2015-06-03 20:36:57 +0200

    dodan kontroler i ruta za pojedinacni event

---
+++ AdriaTicket.com/AdriaTicket.com/Library/Angular/Controllers/AdriaTicketEventController.js
@@ -0,0 +1,26 @@
+﻿
+adriaTicket.controller('EventController', ['$scope', '$location', '$rootElement', '$http','$sce', function ($scope, $location, $rootElement, $http,$sce) {
+
+    var siteUrl = "http://localhost:32718/";
+    var id = $location.absUrl().split('/').pop();
+    $scope.event = {}
+
+    if (parseInt(id)) {
+        $http.get('/public/getEvent/' + id).success(function (data) {
+            $scope.event = data[0];
+            $scope.addEditEvent = "Uredi događaj: " + $scope.event.EVE_Naziv;
+            $scope.saveUpdate = "Spremi";
+            $scope.renderHtml = function (htmlCode) {
+                return $sce.trustAsHtml(htmlCode);
+            };
+            $scope.event.EVE_Datum = moment($scope.event.EVE_Datum).format('DD.MM.YYYY');
+            $scope.event.EVE_DatumOdProdaja = moment($scope.event.EVE_DatumOdProdaja).format('DD.MM.YYYY');
+            $scope.event.EVE_DatumOdPretprodaja = moment($scope.event.EVE_DatumOdPretprodaja).format('DD.MM.YYYY');
+
+        }).error(function () { alert('error event') });
+    }
+
+
+
+
+}]);
